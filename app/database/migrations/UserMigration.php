e35d76380d42e1fee585e6efb7df67a211f3ae7c
Tjoosten <Tjoosten3@gmail.com>
2015-02-09 20:37:04 +0100

    Update

--- app/database/migrations/UserMigration.php
+++ app/database/migrations/UserMigration.php
@@ -8,11 +8,22 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 class UserMigration {
     function run()
     {
-        Capsule::schema()->dropIfExists('users');
-        Capsule::schema()->create('users', function($table) {
-            $table->increments('id');
-            $table->string('username');
-            $table->timestamps();
+        Capsule::schema()->dropIfExists('API');
+
+        Capsule::schema()->create('API', function($table) {
+            $table->increments('SWAPI_id');
+            $table->string('Name');
+            $table->text('Other_names');
+            $table->string('Title');
+            $table->string('Birth');
+            $table->string('Death');
+            $table->string('Weapon');
+            $table->string('Race');
+            $table->string('Culture');
+            $table->string('Gender');
+            $table->string('Height');
+            $table->string('Eye_color');
+            $table->text('Realms_ruled');
         });
     }
 }
