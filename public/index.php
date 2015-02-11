04da481854042af3c0911c985cdb9deab3910087
Tjoosten <Tjoosten3@gmail.com>
2015-02-11 16:21:02 +0100

    update

--- public/index.php
+++ public/index.php
@@ -15,10 +15,6 @@
   # Setting up classes.
   $api  = new Slim\Slim(['mode'  => 'development']);
 
-  # Create log pipeline
-  $log = new Logger('API');
-  $log->pushHandler(new StreamHandler('../app/log/API.log', Logger::INFO));
-
   /**
    * Mode configuration
    */
@@ -46,55 +42,45 @@
   });
 
   # GET www.domain.com/all
-  $api->get('/all', function() use($api, $log) {
+  $api->get('/all', function() use($api) {
     $query = \User::all()
                   ->toJson();
     # Response
-    $log->addInfo(count($query) .' Record(s) retrieved.');
-
     $api->response->headers->set('Content-type', 'application/json');
     $api->response->setBody($query);
   });
 
   # GET www.domain.com/gender/:value
-  $api->get('/gender/:value', function($value) use($api, $log) {
+  $api->get('/gender/:value', function($value) use($api) {
     $query = \User::Where('Gender', '=', $value)
                   ->get()
                   ->toJson();
 
-    if($api->response->getStatus() == 200) {
-      # Response
-      $log->addInfo(count($query) .' Record(s) retrieved.');
-
+    if($api->response->getStatus() === 200) {
       $api->response->headers->set('Content-type', 'application/json');
       $api->response->setBody($query);
     }
   });
 
   # GET www.domain.com/race/:value
-  $api->get('/race/:value', function($value) use($api, $log) {
+  $api->get('/race/:value', function($value) use($api) {
     $query = \User::Where('Race', '=', $value)
                   ->get()
                   ->toJson();
 
-    if($api->response->getStatus() == 200) {
-      # Response
-      $log->addInfo(count($query) .' Record(s) retrieved.');
-
+    if($api->response->getStatus() === 200) {
       $api->response->headers->set('Content-type', 'application/json');
       $api->response->setBody($query);
     }
   });
 
   # GET www.domain.com/:id
-  $api->get('/:id', function($id) use($api, $log) {
+  $api->get('/:id', function($id) use($api) {
     $query = \User::Where('Swapi_id', '=', $id)
                   ->get()
                   ->toJson();
 
     # Response
-    $log->addInfo(count($query) .' Record(s) retrieved.');
-
     $api->response->headers->set('Content-type', 'application/json');
     $api->response->setBody($query);
   });
@@ -103,11 +89,13 @@
    * PUT routes
    */
 
-  # url: www.domain.com/insert/character
+  # url: PUT www.domain.com/insert/character
   # cli: curl -X PUT -d '{"json":123}' http://localhost:8000/test
   # php: <php option>
   $api->put('/test', function() use($api) {
-
+    $user = new \User();
+    $user->Name = "Tim";
+    $user->save();
   });
 
   # Bootstrap everything
